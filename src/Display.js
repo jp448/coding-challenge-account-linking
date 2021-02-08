42868ef89436fb79c295cc2a8c381249c5073bfb
Jocelyn Proctor <jocelynkproctor@gmail.com>
2021-02-08 13:48:34 +0100

    basic components built

---
+++ src/Display.js
@@ -0,0 +1,10 @@
+import './Display.css';
+
+function Display() {
+  return (
+    <div className="Display">
+    </div>
+  );
+}
+
+export default Display;
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+        <p>Account Linking</p>
+    </nav>
+  );
+}
+
+export default Navbar;
